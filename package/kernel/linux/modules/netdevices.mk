0c31fb1a6c0f86b9b2df5c750825ec33631790f5
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2026-09-05 23:58:36 -0300

    airoha: add airoha an8801 and fix polarity on en8801s
    Signed-off-by: Matt Eaton <git@divinehawk.com>
    Signed-off-by: Matheus Sampaio Queiroga <me@sirherobrine23.com.br>
    Signed-off-by: Matheus Sampaio Queiroga <srherobrine20@gmail.com>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -644,10 +644,10 @@ define KernelPackage/phy-airoha-en8811h
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Airoha EN8811H 2.5G Ethernet PHY
   DEPENDS:=+airoha-en8811h-firmware +kmod-libphy
-  KCONFIG:=CONFIG_AIR_EN8811H_PHY
+  KCONFIG:=CONFIG_AIROHA_EN8811H_PHY
   FILES:= \
-   $(LINUX_DIR)/drivers/net/phy/air_en8811h.ko
-  AUTOLOAD:=$(call AutoLoad,18,air_en8811h,1)
+   $(LINUX_DIR)/drivers/net/phy/airoha/en8811h.ko
+   AUTOLOAD:=$(call AutoLoad,18,en8811h,1)
 endef
 
 define KernelPackage/phy-airoha-en8811h/description
